66332c06a3a6c7c963bf1d76548cb0b1afa969d3
fabricalo-web <fabricalo.web@gmail.com>
2025-09-05 10:26:16 -0500

    Proyecto base

---
+++ src/main/java/pe/com/fabricaloweb/report/GreetingConfig.java
@@ -0,0 +1,12 @@
+package pe.com.fabricaloweb.report;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
